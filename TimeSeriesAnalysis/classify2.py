b8ec7ba0a3c4f004f51149e88d548777ecb3b1ef
unknown <jhyinchina@gmail.com>
2016-12-14 15:48:52 +0800

    15:48

--- TimeSeriesAnalysis/classify2.py
+++ TimeSeriesAnalysis/classify2.py
@@ -25,43 +25,124 @@ from keras.layers.normalization import BatchNormalization
 import time
 
 #====================1. getdata===============================================
-# =====采用data。txt的数据，可能存在问题：不同t的排名无法比较 所以抛弃=======
-def get_data(filepath = 'C:\\Users\\Jhy1993\\Desktop\\data\\1.csv'):    
-    data = pd.read_csv(filepath, header=None)
-#    data2 = np.array(list(data))
-    data2 = np.array(data)
-    X = []
-    Y = []
-    print ('www')
-    for i in range(len(data2)):
-        if data2[i, 2] > 0.5:
-            X.append(data2[i, 3:])
-            Y.append(1)
-        else:
-            X.append(data2[i, 3:])
-            Y.append(0)   
-    X = np.array(X)
-    Y = np.array(Y)
-    
-    X_train, Y_train, X_test, Y_test = split_data(X, Y)
-    return X_train, Y_train, X_test, Y_test
-X_train, Y_train, X_test, Y_test = get_data()    
+# =====采用data。txt的数据，存在问题：不同t的排名无法比较 所以抛弃=======
+#def get_data(filepath = 'C:\\Users\\Jhy\\Desktop\\data\\1.csv'):    
+#    data = pd.read_csv(filepath, header=None)
+##    data2 = np.array(list(data))
+#    data2 = np.array(data)
+#    X = []
+#    Y = []
+#    print ('www')
+#    for i in range(len(data2)):
+#        if data2[i, 2] > 0.5:
+#            X.append(data2[i, 3:])
+#            Y.append(1)
+#        else:
+#            X.append(data2[i, 3:])
+#            Y.append(0)   
+#    X = np.array(X)
+#    Y = np.array(Y)
+#    
+#    X_train, Y_train, X_test, Y_test = split_data(X, Y)
+#    return X_train, Y_train, X_test, Y_test
+# 
 # 打标签需要对每个文件的排名分别打标签      
 
-import os
-root = 'C:\\Users\\Jhy1993\\Desktop\\data'
-d = []
-for i in os.listdir(root):
-    if os.path.isfile(os.path.join(root, i)):
-        print (i)
-        print (os.path.join(root, i))
-        d.append(os.path.join(root, i))
-        
-import pandas as pd
-dddd = pd.read_csv(d[0], header=None)
 
 
+def get_data(root, split=500):
+    data_train = []
+    data_test = []
+    for i in os.listdir(root):
+        if os.path.isfile(os.path.join(root, i)):
+    #        print (i)
+    #        print (os.path.join(root, i))
+            if int(i.split('.')[0]) <= split:
+                data_train.append(os.path.join(root, i))
+            else:
+                data_test.append(os.path.join(root, i))
+    return data_train, data_test
 
+           
+def trans_data(data):
+    X = []
+    Y = []
+    for i in range(0, 2):#####range(len(data)):
+        data = pd.read_csv(data_train[i], header=None)
+        data = data.values
+        data = data.astype('float32')
+        for j in range(len(data)):
+            X.append(data[j, 3:])
+            Y.append(0 if data[j, 2] < 0.5 else 1)
+    return X, Y
+
+    
+#for t in range(1, 2):
+#    filename = locals()[str(t) + '.csv']
+#    filepath = os.path.join(root, filename)
+#    data = pd.read_csv(filepath, header=None)
+#    
+#dddd = pd.read_csv(d[0], header=None)
+def DNN(input_dim=763):
+    model = Sequential()
+    
+    model.add(Dense(2000, input_dim=input_dim))
+    
+    model.add(Dense(1000))
+    model.add(Dropout(0.5))
+    model.add(BatchNormalization(epsilon=1e-04))
+    
+    model.add(Dense(500))
+    model.add(Dropout(0.5))
+    model.add(BatchNormalization(epsilon=1e-04))
+    
+    model.add(Dense(200))
+    
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+    #model.summary()
+    model.compile(loss='binary_crossentropy',
+                  optimizer=sgd,
+                  metrics=['accuracy'])
+    return model
+
+def train_model(model, X_train, Y_train, X_test, Y_test, batch_size=32,
+                nb_epoch=100):
+    model.fit(X_train, Y_train, batch_size=batch_size, shuffle=True,
+              nb_epoch=nb_epoch)
+    score, acc = model.evaluate(X_test, Y_test, batch_size=batch_size)
+    print('Test, Score: {}  Test Accuracy: {}'.format(score, acc))
+    return model
+
+def save_model(model, )
+
+    
+if __name__ == '__main__':
+    t1 = time.time()
+    root = 'C:\\Users\\Jhy\\Desktop\\data'
+    data_train, data_test = get_data(root)
+    X_train, Y_train = trans_data(data_train)           
+    X_test, Y_test = trans_data(data_test)
+    Y_t
+    model = DNN()
+    train_model(model, X_train, Y_train, X_test, Y_test)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 # split data
 def split_data(X, Y, rate=0.7):
     sp = int(len(X) * rate)
